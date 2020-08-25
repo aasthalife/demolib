4c1224288786dcd353814ef93ddf036fbb2ce2b0
karar rifat <kararrifat@gmail.com>
2020-08-25 19:58:23 +0600

    first commit

---
+++ src/main/java/com/github/aasthalife/demolib/DemolibApplication.java
@@ -0,0 +1,13 @@
+package com.github.aasthalife.demolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aasthalife/demolib/DemolibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aasthalife.demolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
